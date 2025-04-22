b9c0fb3c59b56fe1a7920098c8137f0f6e17ad53
likp <petros.likidis@scb.se>
2025-04-22 15:33:50 +0200

    First take on file based migration

--- src/sq-migrate/MigrateCommand.cs
+++ src/sq-migrate/MigrateCommand.cs
@@ -1,6 +1,9 @@
-﻿using Spectre.Console;
+﻿using PCAxis.Query;
+using Spectre.Console;
 using Spectre.Console.Cli;
 using System.ComponentModel;
+using System.Text.Json;
+using System.Xml;
 
 namespace sq_migrate
 {
@@ -15,11 +18,15 @@ namespace sq_migrate
 
             [CommandOption("-s|--source-storage-location")]
             [Description("Path where to find old saved queries that should be migrated")]
-            public string? SourceDatabase { get; set; }
+            public string? Source { get; set; }
 
             [CommandOption("-d|--destination-storage-location")]
             [Description("Path where to output migrated saved queries")]
-            public string? DestinationDatabase { get; set; }
+            public string? Destination { get; set; }
+
+            [CommandOption("-p|--store-database-path")]
+            [Description("Path where the Menu.xml and PX files are located")]
+            public string? SourcePath { get; set; }
 
             [CommandOption("-v|--database-vendor")]
             [Description("The type of relational database")]
@@ -31,7 +38,7 @@ namespace sq_migrate
             [DefaultValue("dbo")]
             public string? SourceSchemaOwner { get; set; }
 
-            [CommandOption("-p|--destination-database-schema-owner")]
+            [CommandOption("-u|--destination-database-schema-owner")]
             [Description("The owner of the source database table")]
             [DefaultValue("dbo")]
             public string? DestinationSchemaOwner { get; set; }
@@ -49,6 +56,7 @@ namespace sq_migrate
 
         public override Task<int> ExecuteAsync(CommandContext context, Settings settings)
         {
+
             if (settings.StorageType == StorageTypes.File)
             {
                 return MigrateFileAsync(context, settings);
@@ -64,14 +72,160 @@ namespace sq_migrate
             return Task.FromResult(-1);
         }
 
-        public Task<int> MigrateFileAsync(CommandContext context, Settings settings)
+        public async Task<int> MigrateFileAsync(CommandContext context, Settings settings)
         {
-            var sourceLocation = AssureSourceLocation(settings.SourceDatabase);
+            var sourceLocation = AssureSourceLocation(settings.Source);
+            var destinationLocation = AssureDestinationLocation(settings.Destination);
+            var sourcePath = AssureSourcePath(settings.SourcePath);
 
-            AnsiConsole.Markup($"Source location: [green]{sourceLocation}[/]");
 
-            throw new NotImplementedException();
+
+            AnsiConsole.Markup($"Source location (Saved queries): [green]{sourceLocation}[/]\n");
+            AnsiConsole.Markup($"Destination location: [green]{destinationLocation}[/]\n");
+            AnsiConsole.Markup($"Source path location(PX files): [green]{sourcePath}[/]\n\n");
+
+
+            var lookup = GetMap(sourcePath);
+
+            int counter = 0;
+
+            foreach (var srcFile in Directory.GetFiles(sourceLocation, "*.pxsq", SearchOption.AllDirectories))
+            {
+                var name = Path.GetFileNameWithoutExtension(srcFile);
+                var destFile = Path.Combine(destinationLocation, name.Substring(0, 2), name + ".sq");
+
+                if (File.Exists(destFile))
+                {
+                    AnsiConsole.Markup($"{name} [blue]Already in destionation[/]\n");
+                }
+                else
+                {
+                    // Make sure directory exists for the destination file
+                    if (!Directory.Exists(Path.GetDirectoryName(destFile)))
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(destFile)!);
+                    }
+
+                    // TODO - Convert and save the file
+
+
+                    string query = await File.ReadAllTextAsync(srcFile);
+                    var sq = JsonHelper.Deserialize<PCAxis.Query.SavedQuery>(query) as PCAxis.Query.SavedQuery;
+                    if (sq != null)
+                    {
+                        sq.LoadedQueryName = Path.GetFileNameWithoutExtension(name);
+                    }
+                    else
+                    {
+                        AnsiConsole.Markup($"{name} [red]Failed to parse query[/]\n");
+                        continue;
+                    }
+                    var sqa = Convert(sq, lookup);
+
+                    if (sqa is null)
+                    {
+                        AnsiConsole.Markup($"{name} [red]Failed to convert query[/]\n");
+                        continue;
+                    }
+
+                    var savedQueryString = JsonSerializer.Serialize(sqa);
+                    File.WriteAllText(destFile, savedQueryString);
+
+
+                    counter++;
+                    AnsiConsole.Markup($"{name} [green]Converted[/]\n");
+                }
+            }
+
+            return counter;
         }
+
+        private static Dictionary<string, string> GetMap(string sourcePath)
+        {
+            var lookup = new Dictionary<string, string>();
+            var menuFile = Path.Combine(sourcePath, "Menu.xml");
+
+            var xdoc = new XmlDocument();
+            xdoc.Load(menuFile);
+
+            string xpath = "//Link";
+            var nodeList = xdoc.SelectNodes(xpath);
+
+            if (nodeList != null)
+            {
+                foreach (XmlElement childEl in nodeList)
+                {
+                    string selection = childEl.GetAttribute("selection").Replace('\\', '/');
+                    string tableId = childEl.GetAttribute("tableId").ToUpper();
+                    if (!lookup.ContainsKey(selection))
+                    {
+                        lookup.Add(selection, tableId);
+                    }
+                }
+
+            }
+
+            return lookup;
+        }
+
+
+        private PxWeb.Api2.Server.Models.SavedQuery? Convert(PCAxis.Query.SavedQuery sq, Dictionary<string, string> lookup)
+        {
+            // TODO - Convert the query to the new format
+            var sqa = new PxWeb.Api2.Server.Models.SavedQuery();
+            sqa.Selection = new PxWeb.Api2.Server.Models.VariablesSelection();
+            sqa.Selection.Selection = new List<PxWeb.Api2.Server.Models.VariableSelection>();
+
+
+            sqa.Language = sq.Sources[0].Language;
+            sqa.Id = sq.LoadedQueryName;
+
+            if (lookup.ContainsKey(sq.Sources[0].Source))
+            {
+                sqa.TableId = lookup[sq.Sources[0].Source];
+            }
+            else
+            {
+                return null;
+            }
+
+
+            foreach (var query in sq.Sources[0].Quieries)
+            {
+                var selection = new PxWeb.Api2.Server.Models.VariableSelection();
+                selection.ValueCodes = new List<string>();
+                selection.VariableCode = query.Code;
+                if (query.Selection.Filter.StartsWith("agg:", StringComparison.OrdinalIgnoreCase) || query.Selection.Filter.StartsWith("vs:", StringComparison.OrdinalIgnoreCase))
+                {
+                    selection.CodeList = query.Selection.Filter.Substring(query.Selection.Filter.IndexOf(':'));
+                    selection.ValueCodes.AddRange(query.Selection.Values.ToList());
+                }
+                else if (string.Equals(query.Selection.Filter, "TOP", StringComparison.OrdinalIgnoreCase))
+                {
+                    selection.ValueCodes.Add($"TOP({query.Selection.Values[0]})");
+                }
+                else if (string.Equals(query.Selection.Filter, "ALL", StringComparison.OrdinalIgnoreCase))
+                {
+                    selection.ValueCodes.Add(query.Selection.Values[0]);
+                }
+                else
+                {
+                    selection.ValueCodes.AddRange(query.Selection.Values.ToList());
+                }
+                sqa.Selection.Selection.Add(selection);
+            }
+
+            //Set Placement to the last Pivot operation
+            var op = sq.Workflow.LastOrDefault(s => s.Type == "PIVOT");
+            if (op != null)
+            {
+
+            }
+
+            return sqa;
+        }
+
+
 
         public Task<int> MigrateDatabaseAsync(CommandContext context, Settings settings)
         {
@@ -90,6 +244,23 @@ namespace sq_migrate
 
             return AnsiConsole.Prompt(
                 new TextPrompt<string>("What's the path to the directory where the saved queries are stored?")
+                    .Validate(location
+                        => Directory.Exists(location)
+                            ? ValidationResult.Success()
+                            : ValidationResult.Error("[yellow]Invalid path[/]")));
+
+        }
+
+
+        private static string AssureSourcePath(string? sourcePath)
+        {
+            if (!string.IsNullOrWhiteSpace(sourcePath) && Directory.Exists(sourcePath))
+            {
+                return sourcePath;
+            }
+
+            return AnsiConsole.Prompt(
+                new TextPrompt<string>("What's the path to the directory where the PX files are stored?")
                     .Validate(location
                         => Directory.Exists(location)
                             ? ValidationResult.Success()
